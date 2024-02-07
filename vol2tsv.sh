83202405453f9779bc0e747065542b45a5932fae
Mat X <macvfx@users.noreply.github.com>
2024-02-07 08:11:43 -0800

    Update vol2tsv.sh
    Export all archive tapes contents as tab separated files, skip already created files

---
+++ vol2tsv.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Create TSV from volume list (checking if file exists already before writing a new one)
+#
+#
+# Change the path in case P5 is installed elsewhere
+chatcmd="/usr/local/aw/bin/nsdchat -c"
+output_directory="/Volumes/Backup/AW/TSV"
+
+list=`$chatcmd Volume names`
+
+for i in $list
+do
+    tsv_file="$output_directory/$i.tsv"
+
+    # Check if the TSV file already exists
+    if [ -e "$tsv_file" ]; then
+        # Add your comparison logic here if needed
+        echo "TSV file $tsv_file already exists. Skipping..."
+    else
+        # Create the TSV file if it doesn't exist
+        `$chatcmd Volume $i inventory localhost:$tsv_file ppath size handle btime mtime`
+        echo "Created TSV file for Volume $i at $tsv_file"
+    fi
+done
