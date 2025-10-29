966c546ece47276a1f194b5f0cdbf30c60c6423d
Mat X <macvfx@users.noreply.github.com>
2025-10-29 16:08:39 -0700

    Create jobs-archive-only-sql2csv.sh
    select only archive jobs

---
+++ jobs-archive-only-sql2csv.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Copyright Mat X 2025 - All Rights Reserved
+
+# Export Archiware P5 Jobs Db as CSV file
+# Created by Mat X
+
+timestamp=$(date +%Y%m%d-%H%M%S)
+outfile="/private/tmp/p5-all-Jobs-ExportSelectFields-${timestamp}.csv"
+
+sqlite3 /usr/local/aw/config/joblog/resources.db <<EOF
+.header on
+.mode csv
+.output $outfile
+SELECT label, timeCompleted, volumeList, driveList, savedFiles, numKbytes, planName, indexName FROM Job WHERE lower(label) IN ('archive');
+.quit
+EOF
+
+open "$outfile"
