f762181fc06f03159e82423d9a56342a24f92b83
Mat X <macvfx@users.noreply.github.com>
2025-10-29 22:42:26 -0700

    Update jobs-sql2csv.sh
    out file fix

--- jobs-sql2csv.sh
+++ jobs-sql2csv.sh
@@ -3,10 +3,15 @@
 # Export Archiware P5 Jobs Db as CSV file
 # Created by Mat X
 
+timestamp=$(date +%Y%m%d-%H%M%S)
+outfile="/private/tmp/p5-all-Jobs-ExportSelectFields-${timestamp}.csv"
+
 sqlite3 /usr/local/aw/config/joblog/resources.db <<EOF
 .header on
 .mode csv
-.output p5JobListExportSelectFields.csv
+.output $outfile
 SELECT label, timeCompleted, volumeList, driveList, savedFiles, numKbytes, planName, indexName FROM Job;
 .quit
 EOF
+
+open "$outfile"
